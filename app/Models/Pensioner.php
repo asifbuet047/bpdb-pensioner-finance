b60a7b0d7b2c9e8d44af755169c7a61f17b84c86
asifbuet047 <asifbuet047@gmail.com>
2025-09-08 00:27:55 +0600

    create pensioners table into mysql database and add coloums

---
+++ app/Models/Pensioner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pensioner extends Model
+{
+    /** @use HasFactory<\Database\Factories\PensionerFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_09_07_165737_add_erp_id_name_registar_no_basic_etc_to_pensioners.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pensioners', function (Blueprint $table) {
+            $table->integer('erp_id', false, true);
+            $table->string('name');
+            $table->string('register_no', 50);
+            $table->integer('basic_salary', false, true);
+            $table->integer('medical_allowance', false, true);
+            $table->float('incentive_bonus');
+            $table->string('bank_name');
+            $table->string('account_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pensioners', function (Blueprint $table) {
+            $table->dropColumn(['erp_id', 'name', 'register_no', 'basic_salary', 'medical_allowance', 'incentive_bonus', 'bank_name', 'account_number']);
+        });
+    }
+};
