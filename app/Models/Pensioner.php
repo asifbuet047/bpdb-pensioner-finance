8959741d7fe26c3d8fd8be8cadebe31560610204
asifbuet047 <asifbuet047@gmail.com>
2025-09-09 09:17:58 +0600

    some work done

--- app/Models/Pensioner.php
+++ app/Models/Pensioner.php
@@ -9,4 +9,15 @@ class Pensioner extends Model
 {
     /** @use HasFactory<\Database\Factories\PensionerFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'erp_id',
+        'name',
+        'register_no',
+        'basic_salary',
+        'medical_allowance',
+        'incentive_bonus',
+        'bank_name',
+        'account_number'
+    ];
 }
