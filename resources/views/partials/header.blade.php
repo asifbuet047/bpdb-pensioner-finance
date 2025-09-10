fa67e0583807d21b9ee25893621a4ae53f9dafb3
asifbuet047 <asifbuet047@gmail.com>
2025-09-11 00:47:38 +0600

    remove vite and js related files toa void tailwind and boorstrap conflict

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,13 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-ligh py-3 border" style="background-color: #EF959C">
+    <nav class="navbar navbar-expand-lg navbar-light py-3">
         <div class="container">
-            <a class="navbar-brand" href="/"><span class="fw-bolder text-black">BPDB Pensioner Management
-                    App</span></a>
+            <a class="navbar-brand fw-bolder" href="/">BPDB Pensioner Management App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fw-bold">
                     <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="/resume">All Pensioner</a></li>
                     <li class="nav-item"><a class="nav-link" href="/projects">All Officer</a></li>
