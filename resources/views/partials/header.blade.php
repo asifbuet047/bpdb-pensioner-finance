a1af7698a9ee94afa4266ff7dacacb958a2024b8
asifbuet047 <asifbuet047@gmail.com>
2025-10-07 00:25:54 +0600

    Officers have ADMIN role can see all pensioners but not any officers but SUPER_ADMIN can see both

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -31,7 +31,6 @@
                     @if (request()->cookie('user_role') === 'ADMIN')
                         <li class="nav-item scale-animate"><a class="nav-link" href="/pensioners/all">All Pensioner</a>
                         </li>
-                        <li class="nav-item scale-animate"><a class="nav-link" href="/officers">All Officer</a></li>
                         <li class="nav-item scale-animate"><a class="nav-link" href="/offices">All Office</a></li>
                     @endif
 
