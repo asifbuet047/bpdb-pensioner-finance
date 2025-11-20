3eca47bb503ed2b23b78b28c497c26e519f82784
asifbuet047 <asifbuet047@gmail.com>
2025-11-20 09:51:52 +0600

    erp link url changed

--- config/custom.php
+++ config/custom.php
@@ -3,10 +3,10 @@
 return [
     'ERP_USERNAME' => 'BPDBAdmin',
     'ERP_PASSWORD' => 'CompanyAdmin@123',
-    'ERP_LOGIN_URL' => 'https://bc.bdpowersectorerp.com:5533/Account/Login',
+    'ERP_LOGIN_URL' => 'https://bc1.bdpowersectorerp.com:5533/Account/Login',
     'ERP_ACCEPT_HEADER' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'ERP_USER_AGENT_HEADER' => 'Mozilla/5.0',
-    'ERP_REFERER_HEADER' => 'https://bc.bdpowersectorerp.com:5533',
-    'ERP_GET_ALL_URL' => 'https://bc.bdpowersectorerp.com:5533/api/erpemployee/get-all',
-    'ERP_GET_ALL_COUNT_URL' => 'https://bc.bdpowersectorerp.com:5533/api/erpemployee/get-all-count',
+    'ERP_REFERER_HEADER' => 'https://bc1.bdpowersectorerp.com:5533',
+    'ERP_GET_ALL_URL' => 'https://bc1.bdpowersectorerp.com:5533/api/erpemployee/get-all',
+    'ERP_GET_ALL_COUNT_URL' => 'https://bc1.bdpowersectorerp.com:5533/api/erpemployee/get-all-count',
 ];
