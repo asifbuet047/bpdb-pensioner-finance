897baf95979c157a015c7fc3178c064ca6a5225b
asifbuet047 <asifbuet047@gmail.com>
2025-12-10 16:35:03 +0600

    api routes added for bank setails json response for ajax

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bank;
 use Illuminate\Http\Request;
 
 class BankController extends Controller
 {
-    //
+    public function getBankDetailsByRoutingNumber(Request $request)
+    {
+        $routing_number = $request->query('routing');
+        $bank = Bank::where('routing_number', '=', $routing_number)->first();
+        if ($bank) {
+            return response()->json(['data' => $bank, 'status' => true], 200);
+        } else {
+            return response()->json(['data' => 'No record found', 'status' => false], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BankController;
 use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\PensionerController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/bank', [BankController::class, 'getBankDetailsByRoutingNumber']);
